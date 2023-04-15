4e538996ee4de3078fc7a29a8c879ab1dbf33cc4
David Lyhed Danielsson <davidlyheddanielsson@gmail.com>
2023-04-15 16:34:40 +0200

    feat: add multisampling demo

--- src/graphics/dx12/rasterizer_state.hpp
+++ src/graphics/dx12/rasterizer_state.hpp
@@ -17,4 +17,18 @@ struct RasterizerState
         .ForcedSampleCount = 0,
         .ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF,
     };
+
+    static constexpr D3D12_RASTERIZER_DESC Multisampled{
+        .FillMode = D3D12_FILL_MODE_SOLID,
+        .CullMode = D3D12_CULL_MODE_BACK,
+        .FrontCounterClockwise = true,
+        .DepthBias = 0,
+        .DepthBiasClamp = 0.0f,
+        .SlopeScaledDepthBias = 0.0f,
+        .DepthClipEnable = true,
+        .MultisampleEnable = true,
+        .AntialiasedLineEnable = false,
+        .ForcedSampleCount = 0,
+        .ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF,
+    };
 };
